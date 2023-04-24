8ade6d383c623165726e4c686d76b774fd236768
Raquel Campos <112841246+Raquecoder@users.noreply.github.com>
2023-04-23 22:27:16 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let fighters =[ "❤️","💓", "💗", "♥️", "❤️‍🔥","❣️", "💕"]
+let stageEl=document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+
+fightButton.addEventListener("click",function(){
+    let randomIndexOne = Math.floor(Math.random() * fighters.length)
+
+    let randomIndexTwo = Math.floor(Math.random() * fighters.length)
+
+    stageEl.textContent=fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo];
+   
+
+
+
+
+    
+})
